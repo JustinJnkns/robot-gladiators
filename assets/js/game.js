9e607f92af4d3316dbce63d51eef187d4adc5990
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-08-22 13:27:55 -0400

    random health & attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,14 +30,15 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack - 3,playerAttack)
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -56,7 +57,8 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage =randomNumber(enemyAttack -3,enemyAttack);
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -88,7 +90,7 @@ var startGame = function() {
 
       var pickedEnemyName = enemyNames[i];
 
-      enemyHealth = 50;
+      enemyHealth =randomNumber(40,60);
 
       fight(pickedEnemyName);
 if (playerHealth > 0 && i < enemyNames.length - 1){
@@ -137,11 +139,11 @@ endGame();
 
 
       
-  //SHOP FUNCTION
-  var shop = function() {
-    // ask player what they'd like to do
-    var shopOptionPrompt = window.prompt(
-      "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+ //SHOP FUNCTION
+ var shop = function() {
+  // ask player what they'd like to do
+  var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
     );
 //use switch
 switch(shopOptionPrompt){
@@ -185,8 +187,13 @@ switch(shopOptionPrompt){
         shop();
         break;        
   }
-
   };
-  //3.3.6. stopping point
+
+  //function to generate a random numeric value
+  var randomNumber = function(min,max){
+    var value = Math.floor(Math.random() * (max-min + 1)+ min);
+
+    return value;
+  };
 //START GAME ON LOAD-UP
 startGame();
